b3a046c654400c4f85ca098c41a04378973cba47
jibinmathew69 <jibinmathew.69@gmail.com>
2020-05-10 13:49:53 +0530

    Add map plane method

--- 03 - Mandelbrot Set/jibin/mandelbrot.py
+++ 03 - Mandelbrot Set/jibin/mandelbrot.py
@@ -6,11 +6,16 @@ class Mandelbrot:
 
     def map_plane(self, window_size: tuple, window_low: tuple, window_high: tuple):
 
-        real_low = (window_low[0]*self.plane_low.real) / window_size[0]
-        imag_low = (window_low[1]*self.plane_low.imag)/window_size[1]
+        plane_width = (self.plane_high.real - self.plane_low.real)
+        plane_height = (self.plane_high.imag - self.plane_low.imag)
 
-        real_high = (window_high[0]*self.plane_high.real) / window_size[0]
-        imag_high = (window_high[1]*self.plane_high.imag)/window_size[1]
+        plane_real_low, plane_imag_low = self.plane_low.real, self.plane_low.imag
 
-        self.plane_low = complex(real_low, imag_low)
-        self.plane_high = complex(real_high, imag_high)
+        real_low = (window_low[0]*plane_width) / window_size[0]
+        imag_low = (window_low[1]*plane_height) / window_size[1]
+
+        real_high = (window_high[0]*plane_width) / window_size[0]
+        imag_high = (window_high[1]*plane_height) / window_size[1]
+
+        self.plane_low = complex(plane_real_low + real_low, plane_imag_low + imag_low)
+        self.plane_high = complex(plane_real_low + real_high, plane_imag_low + imag_high)
