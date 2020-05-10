7e59e521dd630f10fc1be5094cfe706cc91cba42
jibinmathew69 <jibinmathew.69@gmail.com>
2020-05-10 22:31:08 +0530

    Test equ

--- 03 - Mandelbrot Set/jibin/mandelbrot_test.py
+++ 03 - Mandelbrot Set/jibin/mandelbrot_test.py
@@ -16,12 +16,21 @@ class TestMandelbrot(unittest.TestCase):
 
     def test_set_resolution(self):
         self.assertEqual(self.mandelbrot.resolution, (1, 1))
-        self.mandelbrot.set_resolution((400,500))
-        self.assertEqual(self.mandelbrot.resolution, (0.0125,0.01))
+        self.mandelbrot.set_resolution((400, 500))
+        self.assertEqual(self.mandelbrot.resolution, (0.0125, 0.01))
 
     def test_tends_inf(self):
         self.assertEqual(self.mandelbrot.tends_inf(complex(0, 1)), False)
         self.assertEqual(self.mandelbrot.tends_inf(complex(0, 2)), True)
+
+    def test_equ(self):
+        z = complex(0, 0)
+        c = complex(-1, 1)
+
+        new_z = self.mandelbrot.equ(z, c)
+
+        self.assertEqual(new_z, self.mandelbrot.equ(z, c))
+
 
 if __name__ == '__main__':
     unittest.main()
