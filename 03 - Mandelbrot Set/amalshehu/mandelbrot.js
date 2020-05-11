07d3c6e0aca8f028afd6c617d61857c275cd288e
amalshehu <amalshehu@gmail.com>
2020-05-11 14:31:56 +0530

    [03 - Mandelbrot Set] Improve graphics

--- 03 - Mandelbrot Set/amalshehu/mandelbrot.js
+++ 03 - Mandelbrot Set/amalshehu/mandelbrot.js
@@ -1,22 +1,26 @@
-const WIDTH = 600
-const HEIGHT = 600
+const WIDTH = 1000
+const HEIGHT = 1000
 const cellSize = 1
 const config = {
   width: WIDTH,
   height: HEIGHT,
-  backgroundColor: PIXI.utils.string2hex("#000000"),
+  backgroundColor: PIXI.utils.string2hex("#DAF7A6"),
+  antialias: true,
   resolution: window.devicePixelRatio || 1,
 }
 const app = new PIXI.Application(config)
 const container = new PIXI.Container()
 
-document.body.appendChild(app.view)
+container.filters = [new PIXI.filters.NoiseFilter()]
+app.stage.scale.x = 1.3
+app.stage.scale.y = 1.3
 
+document.body.appendChild(app.view)
 for (let i = 0; i < WIDTH ** 2; i++) {
   const x = (i % WIDTH) * cellSize
   const y = Math.floor(i / HEIGHT) * cellSize
-  const color = pickMandalbrotColor(x, y)
-  if (color !== "#000000") fillPixel(color, x, y)
+  const color = pickMandelbrotColor(x, y)
+  if (color !== "#DAF7A6") fillPixel(color, x, y)
 }
 
 function fillPixel(color, x, y) {
@@ -28,6 +32,7 @@ function fillPixel(color, x, y) {
   app.stage.addChild(container)
 }
 
+
 function nextIteration(z, [c1, c2] = c) {
   const [x, y] = z.reduce((a, b) => [a ** 2 - b ** 2, 2 * a * b])
   return [x + c1, y + c2]
@@ -36,14 +41,14 @@ function nextIteration(z, [c1, c2] = c) {
 function belongsToMandelbrot(x, y) {
   let z = [0, 0]
   const c = [x, y]
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 100; i++) {
     if (z.reduce((a, b) => a ** 2 + b ** 2) > 4) return false
     z = nextIteration(z, c) //  z(n+1) = z(n)^2+ c
   }
   return true
 }
 
-function pickMandalbrotColor(x, y) {
+function pickMandelbrotColor(x, y) {
   const [a, b] = [x, y].map((m) => (m - WIDTH / 2) / (WIDTH / 4))
-  return belongsToMandelbrot(a, b) ? "#f79800" : "#000000"
+  return belongsToMandelbrot(a, b) ? "#C70039" : "#DAF7A6"
 }
