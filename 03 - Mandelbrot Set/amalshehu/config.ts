6165b656c7c88ff1489dca01a4cc1b96c00b365f
amalshehu <amalshehu@gmail.com>
2020-05-12 16:18:33 +0530

    [03 - Mandelbrot Set] Add config file

---
+++ 03 - Mandelbrot Set/amalshehu/config.ts
@@ -0,0 +1,15 @@
+import { utils } from "pixi.js"
+
+export const WIDTH = 800
+export const HEIGHT = 800
+export const cellSize = 1
+export const scaleFactor = 1.3
+export const loopLimit = 300
+export const config = {
+  width: WIDTH,
+  height: HEIGHT,
+  backgroundColor: utils.string2hex("#DAF7A6"),
+  antialias: true,
+  resolution: window.devicePixelRatio || 1,
+  preserveDrawingBuffer: true,
+}
